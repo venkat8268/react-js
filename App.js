7fda99f9dc8030640e043afd63a393198ceb6e28
venkat8268 <venkatjaddu123700@gmail.com>
2025-05-24 21:06:08 +0530

    installed react, react-dom and creating a simple react code to run

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+    'h1',
+    {
+        id: "heading"
+    },
+    'Rendering heading from react'
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
