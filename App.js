c09bb981dddb7be86ebca6aa0e38b7eadacc689f
venkat8268 <venkatjaddu123700@gmail.com>
2025-05-25 14:42:25 +0530

    Jsx, React elements and Functional components

--- App.js
+++ App.js
@@ -9,5 +9,29 @@ const heading = React.createElement(
     'Rendering heading from react'
 );
 
+// JSX => React.createElement => Object => Html(DOM)
+
+const heading2 = <h2 id="heading2" className="heading2">Heading with Jsx</h2>;
+
+// Functional components
+
+const Title = () => (
+    <h3>Title with Functional component</h3>
+);
+
+const Title2 = () => (
+    <h3>Title with Functional component and different declaration</h3>
+);
+
+
+const HeadingComponent = () => (
+    <div>
+        {heading}
+        <Title />
+        {Title2()}
+        <h1>Heading with functional component</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<HeadingComponent />);
