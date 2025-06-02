e1a71e9a81f23e99163c646243a7a7a62dd4fa33
venkat8268 <venkatjaddu123700@gmail.com>
2025-06-02 21:48:15 +0530

    Search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,48 @@
 import Product from "./Product";
-import {restaurantsList} from "./constants";
+import Search from "./Search";
+import { restaurantsList } from "./constants";
+import { useState } from "react";
+
+function filterRestaurants(searchText, restaurants) {    
+    return restaurants.filter((restaurant) => (
+        restaurant.name.includes(searchText)
+    )
+)}
 
 const Body = () => {
 
+    const [restaurants, setRestaurants] = useState(restaurantsList);
+    const [searchText, setSearchText] = useState('');
+
     return (
-        <div className="products">
-            {restaurantsList.map((restaurant) => {
-                return <Product restaurant={restaurant} name={restaurant.name} key={restaurant.id}/>
-            })}
-        </div>
+        <>
+            {/* <Search /> */}
+            <div className="search-container">
+                <input
+                    type="text"
+                    className="search-product"
+                    value={searchText}
+                    onChange={
+                        (e) => (
+                            setSearchText(e.target.value)
+                        )
+                    }
+                />
+                <button
+                    className="search-button"
+                    onClick={() => (
+                        setRestaurants(filterRestaurants(searchText, restaurants))
+                    )}
+                >
+                    Search
+                </button>
+            </div>
+            <div className="products">
+                {restaurants.map((restaurant) => {
+                    return <Product restaurant={restaurant} name={restaurant.name} key={restaurant.id} />
+                })}
+            </div>
+        </>
     )
 };
 
---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+const Search = () => {
+    return (
+        <div className="search-container">
+            <input type="text" className="search-product" value={''}/>
+            <button className="search-button">Search</button>
+        </div>
+    )
+}
+
+export default Search
