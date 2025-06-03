0db92ddb98639ec1352ce2433c5531a69c403860
venkat8268 <venkatjaddu123700@gmail.com>
2025-06-03 23:11:22 +0530

    Shifting search to a new component

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,47 +4,26 @@ import { restaurantsList } from "./constants";
 import { useState } from "react";
 
 const Body = () => {
+  const [restaurants, setRestaurants] = useState(restaurantsList);
 
-    const [restaurants, setRestaurants] = useState(restaurantsList);
-    const [searchText, setSearchText] = useState('');
-    const [filteredRestaurants, setFilteredRestaurants] = useState(restaurantsList);
+  function handleFilteredRestaurants(restaurantsFromSearch) {
+    setRestaurants(restaurantsFromSearch);
+  }
 
-    function filterRestaurants(searchText, restaurants) {    
-        return restaurants.filter((restaurant) => (
-            restaurant.name.includes(searchText)
-        ))
-    }
-    
-    return (
-        <>
-            {/* <Search /> */}
-            <div className="search-container">
-                <input
-                    type="text"
-                    className="search-product"
-                    value={searchText}
-                    onChange={
-                        (e) => (
-                            setSearchText(e.target.value)
-                        )
-                    }
-                />
-                <button
-                    className="search-button"
-                    onClick={() => (
-                        setFilteredRestaurants(filterRestaurants(searchText, restaurants))
-                    )}
-                >
-                    Search
-                </button>
-            </div>
-            <div className="products">
-                {filteredRestaurants.map((restaurant) => {
-                    return <Product restaurant={restaurant} name={restaurant.name} key={restaurant.id} />
-                })}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Search sendFilteredRestaurant={handleFilteredRestaurants} />
+      <div className="products">
+        {restaurants.map((restaurant) => (
+          <Product
+            restaurant={restaurant}
+            name={restaurant.name}
+            key={restaurant.id}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Body;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,33 @@
-const Search = () => {
-    return (
-        <div className="search-container">
-            <input type="text" className="search-product" value={''}/>
-            <button className="search-button">Search</button>
-        </div>
-    )
-}
+import { restaurantsList } from "./constants";
+import { useState } from "react";
 
-export default Search
+const Search = ({ sendFilteredRestaurant }) => {
+  const [searchText, setSearchText] = useState('');
+
+  function filterRestaurants(searchText, restaurants) {
+    return restaurants.filter((restaurant) =>
+      restaurant.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+  }
+
+  function handleSearch() {
+    const filtered = filterRestaurants(searchText, restaurantsList);
+    sendFilteredRestaurant(filtered); // send to parent
+  }
+
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        className="search-product"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <button className="search-button" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default Search;
