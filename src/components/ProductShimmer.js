4d611366a7b7d3b0b88f8e2e17e6f22c40050881
venkat8268 <venkatjaddu123700@gmail.com>
2025-06-10 21:31:33 +0530

    array map shimmer and install react router

--- src/components/ProductShimmer.js
+++ src/components/ProductShimmer.js
@@ -1,54 +1,12 @@
 const ProductShimmer = () => {
     return (
         <div className="product-shimmer">
-            <div className="product animate">
-                <div className="image-container">
+            {Array(10).fill("").map((e, index) => (
+                <div key={index} className="product animate">
+                    <div className="image-container"></div>
+                    <div className="product-description"></div>
                 </div>
-                <div className="product-description">
-                </div>
-            </div>
-            <div className="product animate">
-                <div className="image-container">
-                </div>
-                <div className="product-description">
-                </div>
-            </div>
-            <div className="product animate">
-                <div className="image-container">
-                </div>
-                <div className="product-description">
-                </div>
-            </div>
-            <div className="product animate">
-                <div className="image-container">
-                </div>
-                <div className="product-description">
-                </div>
-            </div>
-            <div className="product animate">
-                <div className="image-container">
-                </div>
-                <div className="product-description">
-                </div>
-            </div>
-            <div className="product animate">
-                <div className="image-container">
-                </div>
-                <div className="product-description">
-                </div>
-            </div>
-            <div className="product animate">
-                <div className="image-container">
-                </div>
-                <div className="product-description">
-                </div>
-            </div>
-            <div className="product animate">
-                <div className="image-container">
-                </div>
-                <div className="product-description">
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
