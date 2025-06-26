3a8eb2e3a607e2cc2e578279dd15242ed018fcdc
Venkatesh <venkatesh@codebox.co.in>
2025-06-26 12:37:51 +0530

    Create Context and use context in functional / class based components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom"
 import useOnlineStatus from "./hooks/useOnlineStatus"
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
 const Header = () => {
     const isOnline = useOnlineStatus();
-    console.log(isOnline);
-    
-    
+    const userData = useContext(UserContext);
+    console.log(userData);
+        
     return (
         <header className="site-header">
             <div className="site-identity">
@@ -17,10 +19,11 @@ const Header = () => {
                     <li><Link to="/news">News</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                     <li>User status : {isOnline ? "🟢" : "🔴"}</li>
+                    <li className={`font-extrabold ${isOnline ? `text-green-600` : `text-red-600`}`}>{ userData.loggedInUser }</li>
                 </ul>
             </nav>
         </header>
     );
 }
 
-export default Header;
+export default Header; 
